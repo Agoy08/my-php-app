95109b2c732eb258f1df9eba0997df02ab487cf9
yoga <144324322+Agoy08@users.noreply.github.com>
2025-12-01 15:36:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,4 +4,4 @@ require __DIR__ . '/vendor/autoload.php';
 use App\Calculator;
 
 $c = new Calculator();
-echo "2 + 3 = " . $c->add(2,3) . PHP_EOL;
+echo "Webhook sukses!\n" . $c->add(2,3) . PHP_EOL;
